b50b037444045cbf120e23ef3419492320d1bc0d
wmzrvvvdzt-ai <wmzrvvvdzt@privaterelay.appleid.com>
2026-05-08 23:56:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("8789931086:AAGzvmCG68oAO-62d1IIRUk2xHgAgjVWNfE
+")
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
