999cfeb02aba2834105d3f080b2314169404b4e5
wmzrvvvdzt-ai <wmzrvvvdzt@privaterelay.appleid.com>
2026-05-11 19:45:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import asyncio
-
 from aiogram import Bot, Dispatcher
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiohttp_socks import ProxyConnector
 
-from config import TOKEN, CHAT_ID, MIN_DISCOUNT
-from handlers.start import router
+from config import TOKEN
 
+proxy_url = "socks5://SGmGga:EbPsjy@45.91.209.146:10230"
 
 async def main():
-    connector = ProxyConnector.from_url(PROXY)
-    session = AiohttpSession(connector=connector)
+    connector = ProxyConnector.from_url(proxy_url)
+
+    session = AiohttpSession(
+        connector=connector
+    )
 
     bot = Bot(token=TOKEN, session=session)
     dp = Dispatcher()
 
-    dp.include_router(router)
-
-    print("Bot started...")
-
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
