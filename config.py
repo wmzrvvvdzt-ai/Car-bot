6d3997b152fe731fa69d42530117f49fdbf2e018
wmzrvvvdzt-ai <wmzrvvvdzt@privaterelay.appleid.com>
2026-05-11 21:52:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-PROXY = os.getenv("PROXY_URL")
+CHAT_ID = int(os.getenv("CHAT_ID", "0"))
+MIN_DISCOUNT = int(os.getenv("MIN_DISCOUNT", 15))
